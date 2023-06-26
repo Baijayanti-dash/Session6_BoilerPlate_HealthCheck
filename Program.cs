64664a2f6b9e4f729c3f7d65b3b13aa968f87c3f
Baijayanti Priyadarshini <baijayantidash99@gmail.com>
2023-06-26 18:45:43 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+using HealthChecks.UI.Client;
+using Microsoft.EntityFrameworkCore;
+using Session6_BoilerPlate.AppDbContext;
+using Session6_BoilerPlate.HealthChecks;
+using Session6_BoilerPlate.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+string con = builder.Configuration.GetConnectionString("localConnectionString");
+builder.Services.AddDbContext<ProductDb>(p => p.UseSqlServer(con));
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//Adding HealthChecks
+builder.Services
+        .AddHealthChecks()
+        .AddCheck<CustomHealthCheck>(nameof(CustomHealthCheck));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapHealthChecks("/health", new()
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});  //EndPoint 
+app.MapControllers();
+
+app.Run();
